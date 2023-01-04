9cd238c42c64420283f01c34967d4a2066d2a42b
Siegfried Dubois <sdubois@genossh.genouest.org>
2023-01-04 13:48:53 +0100

    repo changes

--- minigraph_create_graph.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#SBATCH --job-name=minigraph_create_graph
-#SBATCH --cpus-per-task=8
-#SBATCH --mem=50G
-#SBATCH --output=LOG_minigraph.log
-
-minigraph="/scratch/sdubois/Stage_M2/minigraph/minigraph"
-
-minigraph -cxggs -t16 $1 $2 $3 > $4
