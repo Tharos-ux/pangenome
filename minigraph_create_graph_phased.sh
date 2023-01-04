e8a0f5f1fdfc5baf8fbbfe10dec2c3352d5d5438
Siegfried Dubois <sidubois@grevillier.irisa.fr>
2023-01-04 13:46:14 +0100

    Phased and unphased scripts

--- minigraph_create_graph_phased.sh
+++ minigraph_create_graph_phased.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#SBATCH --job-name=minigraph_seq_to_graph
+#SBATCH --job-name=minigraph_create_graph
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=50G
 #SBATCH --output=LOG_minigraph.log
 
 minigraph="/scratch/sdubois/Stage_M2/minigraph/minigraph"
 
-$minigraph -cx $1 $2 > $3
+$minigraph -cxggs -t16 $1 $2 $3 > $4
