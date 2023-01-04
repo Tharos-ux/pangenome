f60a3b8806f42a2455ad62002b4b26d47785a5fb
Siegfried Dubois <sidubois@grevillier.irisa.fr>
2023-01-04 13:50:16 +0100

    memory

--- minigraph_create_graph_unphased.sh
+++ minigraph_create_graph_unphased.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=minigraph_create_graph
 #SBATCH --cpus-per-task=8
-#SBATCH --mem=50G
+#SBATCH --mem=80G
 #SBATCH --output=LOG_minigraph.log
 
 minigraph="/scratch/sdubois/Stage_M2/minigraph/minigraph"
--- minigraph_seq_to_graph.sh
+++ minigraph_seq_to_graph.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=minigraph_seq_to_graph
 #SBATCH --cpus-per-task=8
-#SBATCH --mem=50G
+#SBATCH --mem=80G
 #SBATCH --output=LOG_minigraph.log
 
 minigraph="/scratch/sdubois/Stage_M2/minigraph/minigraph"
