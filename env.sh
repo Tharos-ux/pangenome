769535e2d380d41de2f7d8af54244394b541c832
Siegfried Dubois <sidubois@grevillier.irisa.fr>
2023-01-09 14:25:53 +0100

    update env

--- env.sh
+++ env.sh
@@ -9,6 +9,11 @@ conda create -p $WD"/.env" python=3.11
 # activate env to install packages
 conda activate $WD"/.env"
 
+# installing vgtools, minimap2, minigraph
+conda install -c bioconda minimap2
+conda install -c bioconda minigraph
+conda install -c bioconda vg
+
 # installing packages
 python -m pip install -r requirements.txt
 
