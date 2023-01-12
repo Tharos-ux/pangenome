4cc76ae0c4d0148d70c00b29e36ad0cfa74cb3d4
Siegfried Dubois <sidubois@grevillier.irisa.fr>
2023-01-12 09:42:24 +0100

    cactus final round

--- cactus_pipeline.sh
+++ cactus_pipeline.sh
@@ -4,19 +4,9 @@
 #SBATCH --mem=80G
 #SBATCH --output=LOG_cactus.log
 
-. /local/env/envconda.sh
+source /scratch/sdubois/Stage_M2/cactus-bin-v2.4.0/cactus_env/bin/activate
 
-ENV=".env/"
-
-if [ -d "$ENV" ];
-then
-    echo "$ENV conda environment already exists"
-    . /local/env/envhtslib-1.6.sh
-else
-	bash env.sh
-fi
-
-docker run -v $(pwd):/data_docker --rm -it quay.io/comparative-genomics-toolkit/cactus:v2.0.5 cactus-minigraph ./jobstore $1 $2 --reference --mapCores 8 --root mr --binariesMode local
+cactus-minigraph ./jobstore $1 $2 --reference --mapCores 8
 
 # $1 must be a txt file from the format 
 # Diploid sample:
