a6082821b47c17b38f22cb33a9fa001281fa8037
Siegfried Dubois <sidubois@grevillier.irisa.fr>
2023-01-12 09:32:04 +0100

    cactus final round

--- install_cactus.sh
+++ install_cactus.sh
@@ -1,11 +1,14 @@
+REL_TAG=v2.4.0
 tar -xzf "cactus-bin-${REL_TAG}.tar.gz"
 cd "cactus-bin-${REL_TAG}"
 
-virtualenv -p python3.9 cactus_env
+
+. /local/env/envpython-3.9.5.sh
+virtualenv -p python3.9.5 cactus_env
+
 echo "export PATH=$(pwd)/bin:\$PATH" >> cactus_env/bin/activate
 echo "export PYTHONPATH=$(pwd)/lib:\$PYTHONPATH" >> cactus_env/bin/activate
 source cactus_env/bin/activate
-python3 -m pip install -U setuptools pip
+python3 -m pip install -U setuptools pip==21.3.1
 python3 -m pip install -U -r ./toil-requirement.txt
 python3 -m pip install -U .
-cd bin && for i in wigToBigWig faToTwoBit bedToBigBed bigBedToBed bedSort hgGcPercent; do wget -q http://hgdownload.cse.ucsc.edu/admin/exe/linux.x86_64/${i}; chmod ugo+x ${i}; done
