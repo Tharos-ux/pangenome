2d292795238b357619e569c6465fa84b5f80bd98
Siegfried Dubois <sidubois@grevillier.irisa.fr>
2023-01-16 15:21:27 +0100

    fix pipeline

--- cactus_pipeline.sh
+++ cactus_pipeline.sh
@@ -9,18 +9,19 @@ source /scratch/sdubois/Stage_M2/cactus-bin-v2.4.0/cactus_env/bin/activate
 # $1 -> path to a txt file
 # $2 -> output path and name (without extension)
 # $3 -> identifier of reference inside txt file
+# $4 -> a temp name
 
 # destroy any .jobstore
-rm -r ./jobstore_$2
+rm -r ./jobstore_$4
 
 # creating GFA1 file (SV graph)
-cactus-minigraph ./jobstore_$2 $1 $2.gfa --reference $3
+cactus-minigraph ./jobstore_$4 $1 $2.gfa --reference $3
 
 # creating paf map (assembly-to-graph alignments)
-cactus-graphmap ./jobstore_$2 $1 $2.gfa $2.paf  --reference $3 --outputFasta $2.sv.gfa.fa.gz
+cactus-graphmap ./jobstore_$4 $1 $2.gfa $2.paf  --reference $3 --outputFasta $2.sv.gfa.fa.gz
 
 # creating hal (cactus base alignment)
-cactus-align ./jobstore_$2 $1 $2.paf $2.hal --pangenome --outVG --reference $3 
+cactus-align ./jobstore_$4 $1 $2.paf $2.hal --pangenome --outVG --reference $3 
 
 # $1 must be a txt file from the format 
 # Diploid sample:
