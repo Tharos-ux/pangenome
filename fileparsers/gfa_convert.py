1a1d6ca9f6d082a81e862f432841ca49dbc762b4
Siegfried Dubois <sidubois@grevillier.irisa.fr>
2023-01-27 17:41:34 +0100

    push v

--- fileparsers/gfa_convert.py
+++ fileparsers/gfa_convert.py
@@ -44,13 +44,18 @@ def rgfa_to_gfa(input_file: str, output_file: str, p_lines: bool = False, keep_t
                     # datas[6][5:] == origin_sequence
                     link_informations.append(
                         (sub('\D', '', datas[1])+datas[2], sub('\D', '', datas[3])+datas[4], datas[5], datas[6][5:]))
-                    edgelist.append((sub('\D', '', datas[1]), [], sub(
-                        '\D', '', datas[3]), int(datas[6][5:])))
+                    edgelist.append((sub('\D', '', datas[1])+datas[2], [], sub(
+                        '\D', '', datas[3])+datas[4], int(datas[6][5:])))
                 # We don't really know linetype
                 else:
                     gfa_writer.write('\n'+'\t'.join(datas))
 
-        print(len(create_p_lines(edgelist)))
+        dnp: dict = create_p_lines(edgelist)
+        # Writing P-lines
+        for path_number, (origin, path) in enumerate(dnp.items()):
+            gfa_writer.write(
+                f"\nP\t{path_number+number_of_nodes}\t{','.join(path)}\t*\tSR:i:{origin}")
+
         if p_lines:
             # We need to add P-lines at this point
             visited_paths: list = []
@@ -124,24 +129,35 @@ def rgfa_to_gfa(input_file: str, output_file: str, p_lines: bool = False, keep_t
                     f"\nP\t{path_number+number_of_nodes}\t{line}\t{cigar}\tSR:i:{origin}")
 
 
-def create_p_lines(edges: list[tuple]) -> list:
-    """Assuming edges is a four-part tuple list [(edge-a,middle|[],edge-b,sequence-of-edge)] computes the reconstruction of genomes.
+def create_p_lines(edges: list[tuple]) -> dict:
+    """Assuming edges is a four-part tuple list
+    [(edge-a,middle|[],edge-b,sequence-of-edge)]
+    computes the reconstruction of genomes.
 
     Args:
         edges (list[tuple]): edge description
     """
     # Edges in reference are shared.
-    bank_of_edges: list[tuple] = [(start, stop, seq)
-                                  for (start, stop, seq) in edges if seq == 0]
+    bank_of_edges: list[tuple] = [(start, middle, stop, seq)
+                                  for (start, middle, stop, seq) in edges if seq == 0]
 
     # We iterate until convergence, which happens when no sequence could be merged
     while True:
         match iterate_edges(edges, bank_of_edges):
             case None:
-                return edges
+                # We need to complete paths that are not part of the reference with the end and start of the reference sequence
+                paths: dict = {str(seq): [start]+mid+[end]
+                               for (start, mid, end, seq) in edges}
+                refseq: list = paths['0']
+                for id, path in paths.items():
+                    if id != '0':
+                        paths[id] = refseq[0:refseq.index(
+                            paths[id][0])] + paths[id] + refseq[refseq.index(paths[id][-1])+1:]
+                return paths
             case (new_edge, pos_to_edit, pos_to_suppress):
                 edges[pos_to_edit] = new_edge
-                edges.pop(pos_to_suppress)
+                if not pos_to_suppress > len(edges):
+                    edges.pop(pos_to_suppress)
 
 
 def iterate_edges(edges: list[tuple], bank_of_edges: list[tuple]) -> tuple | None:
@@ -154,19 +170,19 @@ def iterate_edges(edges: list[tuple], bank_of_edges: list[tuple]) -> tuple | Non
     Returns:
         tuple: _description_
     """
-    for edge_a in edges:
-        for edge_b in edges + bank_of_edges:
+    for i, edge_a in enumerate(edges):
+        for j, edge_b in enumerate(edges + bank_of_edges):
             if edge_a != edge_b:
                 match (edge_a, edge_b):
                     case ((start, internal_left, bridge_a, seq_a), (bridge_b, internal_right, end, seq_b)) | ((bridge_a, internal_right, end, seq_a), (start, internal_left, bridge_b, seq_b)) if seq_a == seq_b and bridge_a == bridge_b:
                         # Same seq and chained, or same seq and chained and reversed
-                        return (start, internal_left + [bridge_a] + internal_right, end, seq_a)
+                        return (start, internal_left + [bridge_a] + internal_right, end, seq_a), i, j
                     case ((start, internal_left, bridge_a, 0), (bridge_b, internal_right, end, seq)) | ((start, internal_left, bridge_a, seq), (bridge_b, internal_right, end, 0)) | ((bridge_a, internal_right, end, 0), (start, internal_left, bridge_b, seq)) | ((bridge_a, internal_right, end, seq), (start, internal_left, bridge_b, 0)) if bridge_a == bridge_b:
                         # Different seq but chained to ref, or different seq but chained to ref and reversed
-                        return (start, internal_left + [bridge_a] + internal_right, end, seq)
+                        return (start, internal_left + [bridge_a] + internal_right, end, seq), i, j
                     case _:
                         # No edge can be merge
-                        return
+                        pass
 
 
 def deep_chains(path: str, cigar: str, origin: str, list_of_paths: list, max_depth: int) -> tuple:
