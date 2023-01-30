a284a6a68dbb01710ac071090f29a118d2d2c77c
Siegfried Dubois <sidubois@grevillier.irisa.fr>
2023-01-30 11:01:32 +0100

    script paftools

---
+++ scripts_bash/paftools.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH --job-name=PAFleGFA
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=80G
+#SBATCH --output=LOG_paftools.log
+
+. /local/env/envconda.sh
+
+ENV=".env/"
+
+if [ -d "$ENV" ];
+then
+    echo "$ENV conda environment already exists"
+else
+	bash env.sh
+fi
+
+conda activate $ENV
+
+minimap2 -cx asm5 --cs $1 $2 $3 sort -k6,6 -k8,8n paftools.js call -f $1 - > out.vcf
+
+conda deactivate
